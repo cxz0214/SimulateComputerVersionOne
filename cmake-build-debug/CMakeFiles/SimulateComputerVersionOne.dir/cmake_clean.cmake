58c63b3fa21542fcc6ff5a9387518f6fa39c4069
cxz0214 <cxz0214@github.com>
2020-12-21 10:48:56 +0800

    更新

---
+++ cmake-build-debug/CMakeFiles/SimulateComputerVersionOne.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimulateComputerVersionOne.dir/Compile.cpp.obj"
+  "CMakeFiles/SimulateComputerVersionOne.dir/main.cpp.obj"
+  "SimulateComputerVersionOne.exe"
+  "SimulateComputerVersionOne.exe.manifest"
+  "SimulateComputerVersionOne.pdb"
+  "libSimulateComputerVersionOne.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimulateComputerVersionOne.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include <cstring>
+
+using namespace std;
+#include "Compile.h"
+vector<string> readFile(char* filePath){
+    vector<string> readResult;
+    char temp[200];
+    FILE * pFileInput;
+    if((pFileInput =  fopen(filePath, "r"))==NULL){
+        printf("ERROR: cannot open file\n");
+        exit(0);
+    }
+    while(!feof(pFileInput)){
+        fgets(temp,200,pFileInput);
+        if(strchr(temp,'#') != NULL)
+            continue;
+        else
+            readResult.push_back(temp);
+    }
+    return readResult;
+
+}
+int main() {
+    Compile* compile = new Compile();
+    char filePath[] = "D:\\Practice\\Compile\\cmake-build-debug\\sum.txt";
+    vector<string> result;
+    vector<string> compileResult;
+    result = readFile(filePath);
+    compileResult =  compile->getResultOfCompile(result);
+    vector<string>::iterator iterator;
+    for(iterator = compileResult.begin(); iterator != compileResult.end();iterator++){
+        cout<<*iterator;
+    }
+    return 0;
+}
